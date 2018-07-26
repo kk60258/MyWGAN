85f234935b33920910911869bdaa25f410b34462
jason_lai <jason_lai@htc.com>
2018-07-26 17:56:13 +0800

    [Stil failing]

--- WGAN.py
+++ WGAN.py
@@ -8,6 +8,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 """
 """
+'123'.split()
 
 import zipfile
 import sys
@@ -164,12 +165,16 @@ class Discriminator(Net):
     def __call__(self, x):
         with tf.variable_scope(self.name, reuse=tf.AUTO_REUSE) as vs:
             layer = tf.reshape(tensor=x, shape=self.x_shape)
-            layer = tf.layers.conv2d(inputs=layer, filters=64, kernel_size=[3, 3], strides=[1, 1], padding='same', activation=tf.nn.relu, kernel_regularizer=None, name=self.next_layer_name)
-            layer = tf.layers.max_pooling2d(inputs=layer, pool_size=[2, 2], strides=[2, 2], padding='same', name=self.next_layer_name)
-            layer = tf.layers.conv2d(inputs=layer, filters=32, kernel_size=[3, 3], strides=[1, 1], padding='same', activation=tf.nn.relu, kernel_regularizer=None, name=self.next_layer_name)
-            layer = tf.layers.max_pooling2d(inputs=layer, pool_size=[2, 2], strides=[2, 2], padding='same', name=self.next_layer_name)
-            layer = tf.layers.flatten(inputs=layer, name=self.next_layer_name)
-            layer = tf.layers.dense(inputs=layer, activation=tf.nn.relu, units=10, name=self.next_layer_name)
+            layer = tf.layers.conv2d(inputs=layer, filters=64, kernel_size=[5, 5], strides=[2, 2], padding='same', activation=tf.nn.leaky_relu, kernel_regularizer=None, name=self.next_layer_name)
+            #             layer = tf.layers.max_pooling2d(inputs=layer, pool_size=[2, 2], strides=[2, 2], padding='same', name=self.next_layer_name)
+            layer = tf.layers.conv2d(inputs=layer, filters=32, kernel_size=[5, 5], strides=[2, 2], padding='same', activation=tf.nn.leaky_relu, kernel_regularizer=None, name=self.next_layer_name)
+            #             layer = tf.layers.max_pooling2d(inputs=layer, pool_size=[2, 2], strides=[2, 2], padding='same', name=self.next_layer_name)
+            layer = tf.layers.conv2d(inputs=layer, filters=16, kernel_size=[3, 3], strides=[2, 2], padding='same', activation=tf.nn.leaky_relu, kernel_regularizer=None, name=self.next_layer_name)
+            layer = tf.layers.conv2d(inputs=layer, filters=8, kernel_size=[3, 3], strides=[2, 2], padding='same', activation=tf.nn.leaky_relu, kernel_regularizer=None, name=self.next_layer_name)
+            layer = tf.layers.conv2d(inputs=layer, filters=1, kernel_size=[3, 3], strides=[2, 2], padding='same', activation=tf.nn.leaky_relu, kernel_regularizer=None, name=self.next_layer_name)
+            #layer = tf.layers.conv2d(layer, num_outputs=10, kernel_size=3, stride=1, activation_fn=None, normalizer_fn=tf.layers.batch_normalization)
+        #             layer = tf.layers.flatten(inputs=layer, name=self.next_layer_name)
+        #             layer = tf.layers.dense(inputs=layer, activation=tf.nn.relu, units=10, name=self.next_layer_name)
         return layer
 
 
@@ -179,28 +184,40 @@ class Generator(Net):
         self.z_shape = z_shape
         self.name = 'g_net'
 
-    def __call__(self, z, batch_size):
+    def __call__(self, z, batch_size, training=False):
         with tf.variable_scope(self.name, reuse=tf.AUTO_REUSE) as vs:
             #z_shape=[batch_size, self.z_shape[1]]
             #x_shape=[batch_size, self.x_shape[1], self.x_shape[2], self.x_shape[3]]
             layer = tf.reshape(tensor=z, shape=self.z_shape)
-            layer = tf.layers.dense(inputs=layer, activation=tf.nn.sigmoid, units=32 * 32 * 3, name=self.next_layer_name)
+            layer = tf.layers.dense(inputs=layer, activation=tf.nn.relu, units=32 * 32 * 3, name=self.next_layer_name)
             layer = tf.reshape(tensor=layer, shape=[-1, 32, 32, 3])
-            layer = tf.image.resize_nearest_neighbor(images=layer, size=[layer.shape[1] * 2, layer.shape[2] * 2])
-            layer = tf.layers.conv2d_transpose(inputs=layer, filters=32, kernel_size=[3, 3], strides=[1, 1],
-                                               padding='same', activation=tf.nn.sigmoid, name=self.next_layer_name)
-            layer = tf.image.resize_nearest_neighbor(images=layer, size=[layer.shape[1] * 2, layer.shape[2] * 2])
-            layer = tf.layers.conv2d_transpose(inputs=layer, filters=64, kernel_size=[3, 3], strides=[1, 1],
-                                               padding='same', activation=tf.nn.sigmoid, name=self.next_layer_name)
-            layer = tf.image.resize_nearest_neighbor(images=layer, size=[layer.shape[1] * 2, layer.shape[2] * 2])
+            #             layer = tf.image.resize_nearest_neighbor(images=layer, size=[layer.shape[1] * 2, layer.shape[2] * 2])
+            layer = tf.layers.conv2d_transpose(inputs=layer, filters=128, kernel_size=[3, 3], strides=[2, 2],
+                                               padding='same', activation=tf.nn.leaky_relu, name=self.next_layer_name)
+            tf.layers.batch_normalization
+            layer = tf.layers.conv2d_transpose(inputs=layer, filters=64, kernel_size=[3, 3], strides=[2, 2],
+                                               padding='same', activation=tf.nn.leaky_relu, name=self.next_layer_name)
+            #             layer = tf.layers.conv2d_transpose(inputs=layer, filters=64, kernel_size=[3, 3], strides=[2, 2],
+            #                                                padding='same', activation=tf.nn.leaky_relu, name=self.next_layer_name)
+            #             layer = tf.layers.conv2d_transpose(inputs=layer, filters=128, kernel_size=[3, 3], strides=[1, 1],
+            #                                                padding='same', activation=tf.nn.leaky_relu, name=self.next_layer_name)
+            #             layer = layer + tf.layers.conv2d_transpose(inputs=layer, filters=128, kernel_size=[3, 3], strides=[1, 1],
+            #                                                padding='same', activation=tf.nn.leaky_relu, name=self.next_layer_name)
+            #             layer = tf.layers.conv2d_transpose(inputs=layer, filters=90, kernel_size=[3, 3], strides=[2, 2],
+            #                                                padding='same', activation=tf.nn.leaky_relu, name=self.next_layer_name)
+            #             layer = tf.image.resize_nearest_neighbor(images=layer, size=[layer.shape[1] * 2, layer.shape[2] * 2])
+            layer = tf.layers.conv2d_transpose(inputs=layer, filters=100, kernel_size=[3, 3], strides=[2, 2],
+                                               padding='same', activation=tf.nn.leaky_relu, name=self.next_layer_name)
+
+            #             layer = tf.image.resize_nearest_neighbor(images=layer, size=[layer.shape[1] * 2, layer.shape[2] * 2])
+            layer = tf.layers.dropout(layer, rate=0.5, training=training, name=self.next_layer_name)
             layer = tf.layers.conv2d_transpose(inputs=layer, filters=3, kernel_size=[3, 3], strides=[1, 1],
-                                               padding='same', activation=tf.nn.sigmoid, name=self.next_layer_name)
+                                               padding='same', activation=tf.nn.tanh, name=self.next_layer_name)
             #             layer = tf.layers.flatten(inputs=layer, name='flatten_d')
             #             layer = tf.layers.dense(inputs=layer, activation=tf.nn.sigmoid, units=784, name='fc_d2')
             #layer = tf.reshape(tensor=layer, shape=self.x_shape)
             layer = tf.reshape(tensor=layer, shape=self.x_shape)
         return layer
-
 """
 """
 
@@ -208,46 +225,55 @@ tf.reset_default_graph()
 
 X_SHAPE = [-1, 256, 256, 3]
 Z_SHAPE = [-1, 100]
-BATCH_SIZE = 16
+BATCH_SIZE = 32
 
 netD = Discriminator(X_SHAPE)
 netG = Generator(X_SHAPE, Z_SHAPE)
 global_step = tf.train.get_or_create_global_step()
 
 z = tf.placeholder(tf.float32, [None, Z_SHAPE[1]], name='z')
+isTraining = tf.placeholder_with_default(False, shape=[], name='is_training')
 
 real_images = tf.placeholder(tf.float32, [None, X_SHAPE[1], X_SHAPE[2], X_SHAPE[3]], name='x')
-
+real_images = real_images + tf.random_normal(shape=tf.shape(real_images), mean=0.0, stddev=0.1, dtype=tf.float32)
 # generated images
-gen_images = netG(z, BATCH_SIZE)
+gen_images = netG(z, BATCH_SIZE, isTraining)
 
 # get the output from D on the real and fake data
 errD_real = netD(real_images)
 errD_fake = netD(gen_images)
 
 # cost functions
-errD = tf.reduce_mean(errD_real) - tf.reduce_mean(errD_fake)
-errG = tf.reduce_mean(errD_fake)
+errD = tf.reduce_mean(errD_fake) - tf.reduce_mean(errD_real)
+errG = -tf.reduce_mean(errD_fake)
 
 # gradient penalty
-epsilon = tf.random_uniform([], 0.0, 1.0)
+epsilon = 0.6#tf.random_uniform([], 0.0, 1.0)
 #sampling x_hat
 x_hat = real_images*epsilon + (1-epsilon) * gen_images
 d_hat = netD(x_hat)
+gradients2 = tf.gradients(d_hat, x_hat)
 gradients = tf.gradients(d_hat, x_hat)[0]
-slopes = tf.sqrt(tf.reduce_sum(tf.square(gradients), reduction_indices=[1]))
+slopes = tf.sqrt(tf.reduce_sum(tf.square(gradients), axis=[1,2,3]))
+penalty = tf.reduce_mean((slopes - 1.0) ** 2)
+
 lambda_weight = 10 # suggested in paper
-gradient_penalty = lambda_weight * tf.reduce_mean((slopes - 1.0) ** 2)
+gradient_penalty = lambda_weight * penalty
 errD += gradient_penalty
 
+
+slopes2 = tf.sqrt(tf.reduce_sum(tf.square(gradients), reduction_indices=[1]))
+penalty2 = tf.reduce_mean((slopes2 - 1.0) ** 2)
+
+
 # tensorboard summaries
-tf.summary.scalar('d_loss', errD, collections=['train_loss'])
-tf.summary.scalar('g_loss', errG, collections=['train_loss'])
-merged_summary_op = tf.summary.merge_all(key='train_loss')
+# tf.summary.scalar('d_loss', errD, collections=['train_loss'])
+# tf.summary.scalar('g_loss', errG, collections=['train_loss'])
+# merged_summary_op = tf.summary.merge_all(key='train_loss')
 
 with tf.variable_scope('Optimizer', reuse=tf.AUTO_REUSE):
     # optimize G
-    G_train_op = tf.train.AdamOptimizer(learning_rate=1e-5).minimize(errG, var_list=netG.vars, global_step=global_step)
+    G_train_op = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(errG, var_list=netG.vars, global_step=global_step)
 
     # optimize D
     D_train_op = tf.train.AdamOptimizer(learning_rate=1e-5).minimize(errD, var_list=netD.vars)
@@ -256,12 +282,13 @@ with tf.variable_scope('Optimizer', reuse=tf.AUTO_REUSE):
 """
 
 
-TENSORBOARD_PATH = '/tmp/wgan/summary'
-restore_dir = '/tempssd/wgan/restore/'
-max_steps = 1
-test_frequency = 1
 
-n_critic_per_step = 1
+TENSORBOARD_PATH = '/tmp/wgan/summary'
+restore_dir = ''#'/tempssd/wgan/save/2018_Jun_28_15_32_23/model/'#'/tempssd/wgan/save/2018_Jun_28_08_30_01/model/'#'/tempssd/wgan/restore/'
+max_steps = 1000
+test_frequency = 100
+
+n_critic_per_step = 5
 best_test_G_loss = sys.maxsize
 
 datagenerator = DataGenerator(src_path, BATCH_SIZE, X_SHAPE)
@@ -292,41 +319,45 @@ def add_value_summary(name, value, step, summary_writer):
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    best_test_accuracy = 0
+    run_options = tf.RunOptions(report_tensor_allocations_upon_oom = True)
 
     summary_writer = tf.summary.FileWriter(summmary_path, sess.graph)
 
     # restore path not empty
     if os.path.exists(os.path.dirname(restore_dir)):
         # gcloud_load()
-        tf.train.Saver().restore(sess, restore_dir)
+        tf.train.Saver().restore(sess, tf.train.latest_checkpoint(restore_dir))
 
     for _ in range(max_steps):
 
         start = time.time()
 
+        train_images = [] # to keep last training image to evaluate D loss later
         # train the discriminator for n_critic_per_step runs
         for critic_itr in range(n_critic_per_step):
-            train_images = sess.run(next_batch)
+            train_images = sess.run(next_batch, options=run_options)
             batch_z = np.random.normal(-1.0, 1.0, size=[BATCH_SIZE, 100]).astype(np.float32)
-            sess.run(D_train_op, feed_dict={real_images: train_images, z: batch_z})
+            _, p1, p2, D_loss = sess.run([D_train_op, penalty, penalty2, errD], feed_dict={real_images: train_images, z: batch_z})
+            logging.debug("p1 {}, p2 {}, D_loss {}".format(p1, p2, D_loss))
 
         # now train the generator once! use normal distribution, not uniform!!
-        train_images = sess.run(next_batch)
+        # train_images = sess.run(next_batch)
         batch_z = np.random.normal(-1.0, 1.0, size=[BATCH_SIZE, 100]).astype(np.float32)
-        _, D_loss, G_loss, summary, step = sess.run([G_train_op, errD, errG, merged_summary_op, global_step],
-                                                    feed_dict={real_images: train_images, z: batch_z})
 
-        summary_writer.add_summary(summary, step)
+        _, step = sess.run([G_train_op, global_step], feed_dict={z: batch_z, isTraining: True})
+
+        D_loss, G_loss  = sess.run([errD, errG], feed_dict={real_images: train_images, z: batch_z})
 
         time_epoch = time.time() - start
 
+        add_value_summary('G_loss', G_loss, step, summary_writer)
+        add_value_summary('D_loss', D_loss, step, summary_writer)
         add_value_summary('time_epoch', time_epoch, step, summary_writer)
 
         logging.debug("step: {}, D_loss: {}, G_loss: {}, time: {}".format(step, D_loss, G_loss, time_epoch))
 
         if step % test_frequency == 0 or step + 1 == max_steps:
-            test_images = sess.run(next_test_batch)
+            test_images = sess.run(next_test_batch, options=run_options)
             batch_test_z = np.random.normal(-1.0, 1.0, size=[BATCH_SIZE, 100]).astype(np.float32)
             D_test_loss, G_test_loss = sess.run([errD, errG], feed_dict={real_images: test_images, z: batch_test_z})
 
